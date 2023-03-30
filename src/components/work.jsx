452a4bb39d969c1e7e0311c473b5a4b6f6ed3211
Elliot <sbaynes1994@gmail.com>
2023-03-30 14:19:33 -0500

    mobile layout alterations

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -4,18 +4,20 @@ import Data from "../portfolio-data/data.js";
 // function to map through the data and return a card for each project
 export default function Work() {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid justify-items-center md:grid-cols-2 lg:grid-cols-3">
       {Data.map((project) => {
         return (
           <div
-            className="m-2 flex flex-col items-center justify-evenly rounded-md bg-white bg-opacity-30"
+            className="mx-8 my-6 flex flex-col items-center justify-evenly rounded-md bg-white bg-opacity-30 md:mx-4 md:my-3"
             key={project.id}
           >
             <img src={project.src} alt={project.title} className="w-7/12" />
             <h2 className="font-bebas-neue text-2xl font-bold text-deep-purple">
               {project.title}
             </h2>
-            <p className=" text-sm text-mid-purple ">{project.skills}</p>
+            <p className="text-xs text-mid-purple md:text-sm ">
+              {project.skills}
+            </p>
             <div className="flex">
               <a
                 href={project.github}
