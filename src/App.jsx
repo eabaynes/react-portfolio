b20746014f964d60796a4f5c231600a6f825c592
Eli Baynes <sbaynes1994@gmail.com>
2023-03-30 00:27:46 -0500

    app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import BodyContainer from "./components/body-container.jsx";
+import Footer from "./components/Footer.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 export default function App() {
   const [currentPage, setCurrentPage] = React.useState("/");
 
-  const navClickHandler = (event) => {
+  function navClickHandler(event) {
     event.preventDefault();
     setCurrentPage(event.target.getAttribute("href"));
-  };
+  }
 
   return (
-    <>
+    <div>
       <NavBar navClickHandler={navClickHandler} />
       <BodyContainer currentPage={currentPage} />
-    </>
+      <Footer />
+    </div>
   );
 }
