d1f1ed686446f8add0d5c2911556b59448658900
Elliot <sbaynes1994@gmail.com>
2023-03-30 13:09:09 -0500

    adjust aboutme text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -34,9 +34,9 @@ export default function About() {
           <p className="mt-3">
             I'm currently seeking a full-time position as a Front End Developer
             where I can continue to sharpen my skills and grow as a developer.
-            My long term goal is to learn more languages and skills, and
-            eventually become a UX/UI designer with the ability to contribute to
-            the development process.
+            My long term goal is to learn more programming languages and
+            technologies, and eventually become a UX/UI designer with the
+            ability to contribute to the development process.
           </p>
         </div>
         <button className=" ml-10 mt-2 w-max rounded-md bg-deep-purple px-2 py-1 font-bebas-neue text-xl text-white ">
