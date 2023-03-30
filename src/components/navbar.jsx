51c41780d5c76342c0817f03e014056d42f3ce12
Elliot <sbaynes1994@gmail.com>
2023-03-30 01:57:26 -0500

    navbar styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,25 +2,25 @@ import PropTypes from "prop-types";
 
 export default function NavBar(props) {
   return (
-    <nav className="transparent">
-      <img src="./EB.png" alt="logo" />
-      <ul className="flex justify-end">
-        <li>
+    <nav className="transparent flex justify-between p-4">
+      <img className="w-11" src="./EB.png" alt="logo" />
+      <ul className="flex justify-between text-lg text-deep-purple">
+        <li className="mx-5">
           <a href="/" onClick={props.navClickHandler}>
             Home
           </a>
         </li>
-        <li>
+        <li className="mx-5">
           <a href="/about" onClick={props.navClickHandler}>
             About
           </a>
         </li>
-        <li>
+        <li className="mx-5">
           <a href="/contact" onClick={props.navClickHandler}>
             Contact
           </a>
         </li>
-        <li>
+        <li className="mx-5">
           <a href="/work" onClick={props.navClickHandler}>
             Work
           </a>
