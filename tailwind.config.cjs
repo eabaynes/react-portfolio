8cb1a712b02185cdf46001442d60d0f7be20f3f4
Eli Baynes <sbaynes1994@gmail.com>
2023-03-30 00:45:51 -0500

    add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cool-white": "#E3F5F6",
+        "tan": "#DBBEA5",
+        "deep-purple": "#302932",
+        "mid-purple": "#413A4E"
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
