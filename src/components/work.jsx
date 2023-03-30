f96a49e6be387e733744287288a140d8313afb58
Elliot <sbaynes1994@gmail.com>
2023-03-30 13:09:21 -0500

    desktop style components

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -7,12 +7,29 @@ export default function Work() {
     <div className="grid grid-cols-3">
       {Data.map((project) => {
         return (
-          <div className="m-2 flex flex-col" key={project.id}>
-            <img src={project.src} alt={project.title} className="w-max" />
-            <h2>{project.title}</h2>
-            <p>{project.skills}</p>
-            <a href={project.github}>Github</a>
-            <a href={project.deployed}>Deployed</a>
+          <div
+            className="m-2 flex flex-col items-center justify-evenly rounded-md bg-white bg-opacity-30"
+            key={project.id}
+          >
+            <img src={project.src} alt={project.title} className="w-7/12" />
+            <h2 className="font-bebas-neue text-2xl font-bold text-deep-purple">
+              {project.title}
+            </h2>
+            <p className=" text-sm text-mid-purple ">{project.skills}</p>
+            <div className="flex">
+              <a
+                href={project.github}
+                className="mx-1 font-bold hover:text-slate-700"
+              >
+                Github
+              </a>
+              <a
+                href={project.deployed}
+                className="mx-1 font-bold hover:text-purple-800"
+              >
+                Deployed
+              </a>
+            </div>
           </div>
         );
       })}
