88a21f7d17c96a249af97951eaa5c3c60e69ec5c
Elliot <sbaynes1994@gmail.com>
2023-03-30 03:35:31 -0500

    WIP about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,15 +2,20 @@
 export default function About() {
   return (
     <div>
-      <h1>Hi! My name is Elliot</h1>
-      <div>
+      <img src="./Portrait.png" alt="selfie" className="w-72" />
+      <div className="w-max">
+        <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black font-bebas-neue text-6xl text-deep-purple ">
+          Hi! My name is Elliot
+        </h1>
+      </div>
+      <div className="my-3 flex w-2/4 flex-col text-xl">
         <p>
           I'm a former Chef turned Web Developer! After 10 years cooking, I
           decided it was time for a change so I can better manage my health and
-          future. I love to learn, and wound up turning to the world of
-          technology for my next career adventure. My interests lie mostly in
-          front-end development and user experience, but I have done back-end
-          development as well.
+          future. I love to learn and wound up turning to the world of
+          technology- where there is always something to learn. My interests lie
+          mostly in front-end development and user experience, but I have done
+          back-end development as well.
         </p>
         <p>
           My experience in the hospitality industry has given me a strong set of
